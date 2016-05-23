8f6e4314f8b2d96f573ad1f3a4755d7fe1f6cf69
Liam Dinsdale <kastoli@gmail.com>
2016-05-23 13:15:48 +1000

    Changes

--- CodeBlocksProject/BruteForceMedian.hpp
+++ CodeBlocksProject/BruteForceMedian.hpp
@@ -23,4 +23,26 @@ int BruteForceMedian(int A[], int n){
     }
 }
 
+int BruteForceMedian_BasicOperations(int A[], int n, unsigned long numoperations){
+    numoperations++;
+    int k = ceil((float)n/2);
+    for(int i = 0; i <= n-1; i++){
+        int numsmaller = 0;
+        int numequal = 0;
+        for(int j = 0; j <= n-1; j++){
+            numoperations++; // Increment basic operation here.
+            if(A[j] < A[i]){ // This is basic operation?
+                numsmaller = numsmaller+1;
+            } else {
+                if(A[j] == A[i]){
+                    numequal = numequal+1;
+                }
+            }
+        }
+        if(numsmaller < k && k <= (numsmaller+numequal)){
+            return A[i];
+        }
+    }
+}
+
 #endif // BRUTEFORCEMEDIAN_HPP_INCLUDED
--- CodeBlocksProject/main.cpp
+++ CodeBlocksProject/main.cpp
@@ -8,15 +8,21 @@ using namespace std;
 int main()
 {
     int ordered_array[10] = {1,2,3,4,5,6,7,8,9,10}; // 5 for BruteForceMedian, 6 for Median
-    int unordered_array[10] = {7,3,10,4,1,8,2,5,9,6}; // 5 for BruteForceMedian, 6 for Median
+    int unordered_array[11] = {7,3,10,4,1,8,2,5,9,6,11}; // 5 for BruteForceMedian, 6 for Median
 
-    int brute_ordered = BruteForceMedian(ordered_array, 10);
-    int brute_unordered = BruteForceMedian(unordered_array, 10);
+    unsigned long basic_operations = 0;
+
+    int brute_ordered = BruteForceMedian_BasicOperations(ordered_array, 10, basic_operations);
+    int brute_unordered = BruteForceMedian_BasicOperations(unordered_array, 11, basic_operations);
     int median_ordered = Median(ordered_array, 10);
     int median_unordered = Median(unordered_array, 10);
 
     cout << brute_ordered << endl;
+    cout << basic_operations << endl;
+
     cout << brute_unordered << endl;
+    cout << basic_operations << endl;
+
     cout << median_ordered << endl;
     cout << median_unordered << endl;
 
