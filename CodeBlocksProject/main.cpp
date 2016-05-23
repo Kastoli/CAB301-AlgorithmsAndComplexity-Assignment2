b2ec6a4eb1b0c5546a5f852a6a6fa58dbabf91b0
Liam Dinsdale <kastoli@gmail.com>
2016-05-23 14:52:59 +1000

    Update

--- CodeBlocksProject/main.cpp
+++ CodeBlocksProject/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <fstream>
 
 #include "BruteForceMedian.hpp"
 #include "Median.hpp"
@@ -13,29 +14,56 @@ int Select_BasicOperations(int A[], int l, int m, int h);
 int Partition_BasicOperations(int A[], int l, int h);
 
 unsigned long numoperations = 0;
-unsigned long numticks = 0;
 
 int main()
 {
+    ofstream myfile;
+    myfile.open ("output.csv");
+    srand(time(NULL));
+    for(int i = 0; i < 10; i++){
+        int test_array[10000];
+        for(int j = 0; j < 10000; j++){
+            test_array[j] = rand()%1000;
+        }
+        clock_t ticks = clock();
+        int median_value = BruteForceMedian_BasicOperations(test_array, 10000);
+        ticks = clock() - ticks;
+        myfile << "10000" << "," << numoperations << "," << ticks << "\n";
+    }
+    myfile.close();
+
+    /*
     int ordered_array[10] = {1,2,3,4,5,6,7,8,9,10}; // 5 for BruteForceMedian, 6 for Median
     int unordered_array[10] = {7,3,10,4,1,8,2,5,9,6}; // 5 for BruteForceMedian, 6 for Median
 
+    clock_t ticks = clock();
     int brute_ordered = BruteForceMedian_BasicOperations(ordered_array, 10);
-    int brute_unordered = BruteForceMedian_BasicOperations(unordered_array, 10);
-    int median_ordered = Median(ordered_array, 10);
-    int median_unordered = Median(unordered_array, 10);
-
+    ticks = clock() - ticks;
     cout << brute_ordered << endl;
     cout << numoperations << endl;
+    cout << ticks << endl << endl;
 
+    ticks = clock();
+    int brute_unordered = BruteForceMedian_BasicOperations(unordered_array, 10);
+    ticks = clock() - ticks;
     cout << brute_unordered << endl;
     cout << numoperations << endl;
+    cout << ticks << endl << endl;
 
+    ticks = clock();
+    int median_ordered = Median_BasicOperations(ordered_array, 10);
+    ticks = clock() - ticks;
     cout << median_ordered << endl;
     cout << numoperations << endl;
+    cout << ticks << endl << endl;
 
+    ticks = clock();
+    int median_unordered = Median_BasicOperations(unordered_array, 10);
+    ticks = clock() - ticks;
     cout << median_unordered << endl;
     cout << numoperations << endl;
+    cout << ticks << endl << endl;
+    */
 
     return 0;
 }
@@ -67,20 +95,20 @@ int Median_BasicOperations(int A[], int n){
     if(n == 1){
         return A[0];
     } else {
-        Select(A, 0, floor((float)n/2), n-1);
+        Select_BasicOperations(A, 0, floor((float)n/2), n-1);
     }
 }
 
 int Select_BasicOperations(int A[], int l, int m, int h){
-    int pos = Partition(A, l, h);
+    int pos = Partition_BasicOperations(A, l, h);
     if(pos == m){
         return A[pos];
     }
     if(pos > m){
-        return Select(A, l, m, pos-1);
+        return Select_BasicOperations(A, l, m, pos-1);
     }
     if(pos < m){
-        return Select(A, pos+1, m, h);
+        return Select_BasicOperations(A, pos+1, m, h);
     }
 }
 
